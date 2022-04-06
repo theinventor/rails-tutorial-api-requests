e001a0682fd9095fecf41e9bac20e118e796bd28
Troy Anderson <the.inventor@gmail.com>
2022-04-06 11:54:32 -0700

    add api request and secure credential storage

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.1.0"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.2", ">= 7.0.2.3"
+gem 'faraday'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- app/controllers/nasa_images_controller.rb
+++ app/controllers/nasa_images_controller.rb
@@ -1,4 +1,6 @@
 class NasaImagesController < ApplicationController
   def welcome
+    client = Faraday.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=#{Rails.application.credentials.nasa_key}")
+    @images = JSON.parse(client.body)['photos']
   end
 end
